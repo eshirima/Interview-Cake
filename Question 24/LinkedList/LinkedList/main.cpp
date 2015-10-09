47a7376740680b7fcf5a342798160e45e458e8e6
eshirima <eshirima@kent.edu>
2015-10-08 23:32:08 -0400

    Created the project files
    Added the source code folder along with a text file with the respective
    question

---
+++ Question 24/LinkedList/LinkedList/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LinkedList
+//
+//  Created by Emil Shirima on 10/8/15.
+//  Copyright (c) 2015 Emil Shirima. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
